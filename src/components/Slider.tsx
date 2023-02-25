23c33ec3360d63389c4db283781b579dd433285f
Igor Shchetinkin <fastigor293015@gmail.com>
2023-02-25 15:32:07 +1000

    pagination bullets animation + mainPage

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -4,10 +4,11 @@ import { ArrowBack, ArrowForward } from "@mui/icons-material";
 import { motion, AnimatePresence, wrap } from "framer-motion";
 
 interface ISliderProps {
+  height: number | string;
   imagesList: string[];
 }
 
-const variants = {
+const slideVariants = {
   enter: (direction: number) => {
     return {
       x: direction > 0 ? 1000 : -1000,
@@ -28,12 +29,16 @@ const variants = {
   }
 }
 
+// const paginationVariants = {
+//   enter: ()
+// }
+
 const swipeConfidenceThreshold = 10000;
 const swipePower = (offset: number, velocity: number) => {
   return Math.abs(offset) * velocity;
 }
 
-const Slider = ({ imagesList }: ISliderProps) => {
+const Slider = ({ height, imagesList }: ISliderProps) => {
   const [[slide, direction], setSlide] = useState([0, 0]);
   const { palette } = useTheme();
 
@@ -50,16 +55,27 @@ const Slider = ({ imagesList }: ISliderProps) => {
     <Box
       position="relative"
       mb="50px"
+      // sx={{
+      //   overflowX: "hidden",
+      //   overflowY: "visible",
+      // }}
     >
       {/* Wrapper */}
       <Box
         position="relative"
-        height="500px"
+        height={height}
         borderRadius="15px"
         bgcolor="rgb(226 232 240)"
         boxShadow="0px 0px 20px 7px rgba(34, 60, 80, 0.2)"
         overflow="hidden"
+        component={motion.div}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{
+          delay: .2,
+        }}
       >
+        {/* Slide */}
         <AnimatePresence
           initial={false}
           custom={direction}
@@ -74,7 +90,7 @@ const Slider = ({ imagesList }: ISliderProps) => {
             bgcolor="rgb(239, 242, 19)"
             component={motion.div}
             custom={direction}
-            variants={variants}
+            variants={slideVariants}
             initial="enter"
             animate="center"
             exit="exit"
@@ -110,65 +126,109 @@ const Slider = ({ imagesList }: ISliderProps) => {
         </AnimatePresence>
       </Box>
 
-      <IconButton
-        onClick={() => paginate(-1)}
+
+      {/* Navigation buttons */}
+      <Box
+        position="absolute"
+        top="50%"
+        right="0"
+        left="0"
+        p="0 20px"
+        zIndex={1}
+        display="flex"
+        justifyContent="space-between"
+        overflow="hidden"
         sx={{
-          position: "absolute",
-          top: "50%",
-          zIndex: 1,
-          left: 20,
-          bgcolor: "#FFF",
           transform: "translateY(-50%)",
-          "&:hover": {
-            bgcolor: "rgba(255, 255, 255, .5)"
+          pointerEvents: "none",
+          "& > *": {
+            pointerEvents: "all",
           }
         }}
       >
-        <ArrowBack sx={{ fontSize: "25px", color: "#000" }} />
-      </IconButton>
+        <motion.div
+          initial={{ x: -60 }}
+          animate={{ x: 0 }}
+          transition={{ delay: .5 }}
+        >
+          <IconButton
+            onClick={() => paginate(-1)}
+            sx={{
+              bgcolor: "#FFF",
+              "&:hover": {
+                bgcolor: "rgba(255, 255, 255, .5)"
+              }
+            }}
+          >
+            <ArrowBack sx={{ fontSize: "25px", color: "#000" }} />
+          </IconButton>
+        </motion.div>
 
-      <IconButton
-        onClick={() => paginate(1)}
-        sx={{
-          position: "absolute",
-          top: "50%",
-          zIndex: 1,
-          right: 20,
-          bgcolor: "#FFF",
-          transform: "translateY(-50%)",
-        }}
-      >
-        <ArrowForward sx={{ fontSize: "25px", color: "#000" }} />
-      </IconButton>
+        <motion.div
+          initial={{ x: 60 }}
+          animate={{ x: 0 }}
+          transition={{ delay: .5 }}
+        >
+          <IconButton
+            onClick={() => paginate(1)}
+            sx={{
+              bgcolor: "#FFF",
+              "&:hover": {
+                bgcolor: "rgba(255, 255, 255, .5)"
+              }
+            }}
+          >
+            <ArrowForward sx={{ fontSize: "25px", color: "#000" }} />
+          </IconButton>
+        </motion.div>
+      </Box>
 
       {/* Pagination */}
-      <Box
-        position="absolute"
-        bottom="-35px"
-        left="50%"
-        zIndex={1}
-        display="flex"
-        gap="5px"
-        sx={{
-          transform: "translateX(-50%)",
-        }}
-      >
-        {imagesList.map((item, i) => (i + 2 >= imageIndex) && (i - 2 <= imageIndex) ? (
-          <Box
-            key={`${i}`}
-            width="15px"
-            height="15px"
-            border={`2px solid ${palette.text.primary}`}
-            borderRadius="50%"
-            bgcolor={i === imageIndex ? palette.text.primary : "transparent"}
-            sx={{
-              transform: `scale(${(Math.abs(imageIndex - i) === 1) ? .66 : (Math.abs(imageIndex - i) === 2) ? .33 : 1 })`,
-              cursor: "pointer",
-            }}
-            onClick={() => setSlide([i, 1])}
-          />
-        ) : '')}
-      </Box>
+      <AnimatePresence>
+        <Box
+          position="absolute"
+          bottom="-35px"
+          left="50%"
+          right="0"
+          width="15px"
+          height="15px"
+          zIndex={1}
+          sx={{
+            transform: "translateX(-50%)",
+          }}
+        >
+          {imagesList.map((item, i) => (i + 2 >= imageIndex) && (i - 2 <= imageIndex) ? (
+            <Box
+              key={`${i}`}
+              position="absolute"
+              border={`2px solid ${palette.text.primary}`}
+              borderRadius="50%"
+              sx={{
+                inset: 0,
+                cursor: "pointer",
+              }}
+              onClick={() => setSlide([i, i > imageIndex ? 1 : -1])}
+              component={motion.div}
+              initial={{
+                scale: 0,
+                x: 10 * (i - imageIndex),
+                backgroundColor: "transparent",
+              }}
+              animate={{
+                scale: (Math.abs(imageIndex - i) === 1) ? .70 : (Math.abs(imageIndex - i) === 2) ? .4 : 1,
+                x: 20 * (i - imageIndex),
+                backgroundColor: i === imageIndex ? palette.text.primary : "transparent",
+              }}
+              exit={{
+                scale: 0,
+              }}
+              transition={{
+                delay: .1,
+              }}
+            />
+          ) : '')}
+        </Box>
+      </AnimatePresence>
     </Box>
   )
 }
--- src/scenes/mainPage/index.tsx
+++ src/scenes/mainPage/index.tsx
@@ -1,8 +1,11 @@
-import { Box } from "@mui/material";
-import { motion } from "framer-motion";
+import { Box, Button, Typography, useMediaQuery } from "@mui/material";
 import { useAppSelector } from "../../app/hooks";
+import { motion } from "framer-motion";
 import Container from "../../components/Container";
 import Slider from "../../components/Slider";
+import { ArrowForward } from "@mui/icons-material";
+import PrimaryButton from "../../components/PrimaryButton";
+import { useNavigate } from "react-router-dom";
 
 const images = [
   "https://d33wubrfki0l68.cloudfront.net/dd23708ebc4053551bb33e18b7174e73b6e1710b/dea24/static/images/wallpapers/shared-colors@2x.png",
@@ -12,26 +15,34 @@ const images = [
 
 const MainPage = () => {
   const productsImages = useAppSelector(state => state.products.list).map(item => item.image);
+  const navigate = useNavigate();
+  const isNonMobileScreen = useMediaQuery("(min-width:600px)");
 
   return (
-    <Container>
-      <Box
-        position="absolute"
-        left="0"
-        height="200px"
-        width="200px"
-        boxShadow="0px 0px 20px 7px rgba(34, 60, 80, 0.2)"
-        component={motion.div}
-        drag
-        dragSnapToOrigin
-        whileHover={{ borderRadius: "50%" }}
-        whileFocus={{ borderRadius: "50%" }}
-        whileTap={{ scale: .8 }}
-      ></Box>
-
-      <Slider imagesList={productsImages} />
-
-    </Container>
+    <motion.div animate={{ x: 0, opacity: 1 }} initial={{ x: "50%", opacity: 0 }} exit={{ x: "50%", opacity: 0 }} transition={{ type: "keyframes" }}>
+      <Container>
+        <Box display={"flex"} alignItems="center" justifyContent="space-between" mb="30px">
+          <Typography variant={isNonMobileScreen ? "h2" : "h3"} fontWeight="700">
+            Trending products
+          </Typography>
+          <PrimaryButton
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              gap: "5px",
+              p: "8px 20px",
+              // color: "rgb(67 56 202)",
+              // bgcolor: "transparent",
+            }}
+            onClick={() => navigate("/products")}
+          >
+            {isNonMobileScreen ? "Go to all products" : ""}
+            <ArrowForward />
+          </PrimaryButton>
+        </Box>
+        <Slider height={isNonMobileScreen ? "500px" : "300px"} imagesList={productsImages} />
+      </Container>
+    </motion.div>
   )
 };
 

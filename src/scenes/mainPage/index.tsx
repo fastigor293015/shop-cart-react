1e8d2e287dea94864344883802fa19c8d38bde59
Igor Shchetinkin <fastigor293015@gmail.com>
2023-02-27 22:00:22 +1000

    added fetchProducts on main page

--- src/scenes/mainPage/index.tsx
+++ src/scenes/mainPage/index.tsx
@@ -1,11 +1,13 @@
-import { Box, Button, Typography, useMediaQuery } from "@mui/material";
-import { useAppSelector } from "../../app/hooks";
+import { Box, Button, Skeleton, Typography, useMediaQuery } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { motion } from "framer-motion";
 import Container from "../../components/Container";
 import Slider from "../../components/Slider";
 import { ArrowForward } from "@mui/icons-material";
 import PrimaryButton from "../../components/PrimaryButton";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { fetchProducts } from "../../features/products/productsSlice";
 
 const images = [
   "https://d33wubrfki0l68.cloudfront.net/dd23708ebc4053551bb33e18b7174e73b6e1710b/dea24/static/images/wallpapers/shared-colors@2x.png",
@@ -14,33 +16,49 @@ const images = [
 ]
 
 const MainPage = () => {
-  const productsImages = useAppSelector(state => state.products.list).map(item => item.image);
+  const { list, loading} = useAppSelector(state => state.products);
+  const productsImages = list.map(item => item.image);
+  const dispatch = useAppDispatch();
+  console.log(productsImages);
+
   const navigate = useNavigate();
   const isNonMobileScreen = useMediaQuery("(min-width:600px)");
+
+  useEffect(() => {
+    if (!list || list.length === 0) dispatch(fetchProducts());
+  }, []);
 
   return (
     <motion.div animate={{ x: 0, opacity: 1 }} initial={{ x: "50%", opacity: 0 }} exit={{ x: "50%", opacity: 0 }} transition={{ type: "keyframes" }}>
       <Container>
-        <Box display={"flex"} alignItems="center" justifyContent="space-between" mb="30px">
+        <Box display="flex" alignItems="center" justifyContent="space-between" mb="30px">
           <Typography variant={isNonMobileScreen ? "h2" : "h3"} fontWeight="700">
             Trending products
           </Typography>
-          <PrimaryButton
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              gap: "5px",
-              p: "8px 20px",
-              // color: "rgb(67 56 202)",
-              // bgcolor: "transparent",
-            }}
-            onClick={() => navigate("/products")}
-          >
-            {isNonMobileScreen ? "Go to all products" : ""}
-            <ArrowForward />
-          </PrimaryButton>
+          {loading
+            ? <Skeleton
+            variant="rectangular"
+            width={isNonMobileScreen ? "205px" : "64px"}
+            height="36px"
+            sx={{ borderRadius: "4px", bgcolor: "rgb(67 56 202)" }} />
+            : <PrimaryButton
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: "5px",
+                p: "8px 20px",
+                // color: "rgb(67 56 202)",
+                // bgcolor: "transparent",
+              }}
+              onClick={() => navigate("/products")}
+            >
+              {isNonMobileScreen ? "Go to all products" : ""}
+              <ArrowForward />
+            </PrimaryButton>}
         </Box>
-        <Slider height={isNonMobileScreen ? "500px" : "300px"} imagesList={productsImages} />
+        {loading
+          ? <Skeleton variant="rectangular" height={isNonMobileScreen ? "500px" : "300px"} sx={{ borderRadius: "15px", bgcolor: "rgb(239, 242, 19)" }} />
+          : <Slider height={isNonMobileScreen ? "500px" : "300px"} imagesList={productsImages} />}
       </Container>
     </motion.div>
   )
